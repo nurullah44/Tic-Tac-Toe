32120f006dc8ffe17c7e8b1458a3fce6c3f04047
Nurullah Kuş <nurullahkus68@gmail.com>
2023-12-14 12:47:50 +0300

    Add files via upload
    Stage 2

---
+++ S2_getGamePosition.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val x = readln() // get game position from the user
+    println("---------")
+    println("| ${x[0]} ${x[1]} ${x[2]} |\n| ${x[3]} ${x[4]} ${x[5]} |\n| ${x[6]} ${x[7]} ${x[8]} |")
+    println("---------")
+}
